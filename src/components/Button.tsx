57935d89da1b4e02a12e5ad58671c152f740cea0
Myrian Katto <myriankatto@gmail.com>
2021-04-20 18:55:45 +0200

    navigation, external fonts, icons, views

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,31 +1,31 @@
 import React from 'react';
-import { Text, TouchableOpacity, StyleSheet, TouchableOpacityProps } from 'react-native';
+import { TouchableOpacity, Text, StyleSheet, TouchableOpacityProps } from 'react-native';
+
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 interface ButtonProps extends TouchableOpacityProps {
   title: string;
 }
+
 export function Button({ title, ...rest }: ButtonProps) {
   return (
-    <TouchableOpacity style={styles.button} activeOpacity={0.7} {...rest}>
-      <Text style={styles.buttonText}>{title}</Text>
+    <TouchableOpacity style={styles.container} {...rest}>
+      <Text style={styles.text}>{title}</Text>
     </TouchableOpacity>
   );
 }
-
 const styles = StyleSheet.create({
-  button: {
+  container: {
     backgroundColor: colors.green,
+    height: 56,
+    borderRadius: 16,
     justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 16,
-    marginBottom: 10,
-    height: 56,
-    width: 56,
   },
-
-  buttonText: {
+  text: {
+    fontSize: 16,
     color: colors.white,
-    fontSize: 20,
+    fontFamily: fonts.heading,
   },
 });
---
+++ src/pages/Confirmation.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
+
+import { Button } from '../components/Button';
+
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+export function Confirmation() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.emoji}>🌳</Text>
+
+        <Text style={styles.title}>Pronto!{'\n'}</Text>
+        <Text style={styles.subtitle}>Agora vamos começar a cuidar das suas plantas!{'\n'}</Text>
+        <View style={styles.footer}>
+          <Button title="Começar" />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: 'center', justifyContent: 'space-around' },
+  content: { flex: 1, justifyContent: 'center', alignItems: 'center', width: '100%', padding: 30 },
+  emoji: { fontSize: 78 },
+  title: {
+    fontSize: 22,
+    textAlign: 'center',
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 38,
+    marginTop: 15,
+  },
+  subtitle: {
+    fontSize: 17,
+    textAlign: 'center',
+    color: colors.heading,
+    fontFamily: fonts.text,
+    paddingVertical: 10,
+  },
+  footer: {
+    width: '100%',
+    paddingHorizontal: 50,
+    marginTop: 20,
+  },
+});
