e9ce18c5722c9419eedacc9a1703ae1eaaf790f4
Myrian Katto <myriankatto@gmail.com>
2021-05-07 17:37:56 +0200

    api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/myriankatto/plant-manager',
+  baseURL: 'https://planteria-server.herokuapp.com',
 });
 
 export default api;
